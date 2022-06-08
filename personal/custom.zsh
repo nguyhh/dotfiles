6da07a7eb41247f161f6076c18c30c0f44afeb66
Leena Hai Ha Nguyen <leena.nguyen@shopify.com>
2022-06-08 19:42:36 -0400

    Add plugins to custom.zsh

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,10 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+plugins=(git
+	ruby
+	rails
+	fzf
+    zsh-syntax-highlighting
+	zsh-autosuggestions)
